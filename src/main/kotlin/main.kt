1568efca3c0df84aa6763823368eeff74c9700aa
Jim Bracewell <jim.bracewell@moveguides.com>
2017-10-21 14:26:47 +0100

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+package main.kotlin
+
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.response.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
